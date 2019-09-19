0af8f4ff66b97b8ebf08b610c7a51f0be3800bab
biejunyang <biejunyang1210@163.com>
2019-09-19 21:42:15 +0800

    ccc

--- orchid-examples/docker-example/src/main/java/com/orchid/samples/docker/DockerExampleApp.java
+++ orchid-examples/docker-example/src/main/java/com/orchid/samples/docker/DockerExampleApp.java
@@ -3,14 +3,16 @@ package com.orchid.samples.docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class MainApp {
+@EnableDiscoveryClient
+public class DockerExampleApp {
     public static void main(String[] args){
-        SpringApplication.run(MainApp.class, args);
+        SpringApplication.run(DockerExampleApp.class, args);
     }
 
 
